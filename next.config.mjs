6fa62c0fc6dc0dea2d59af64b15852c8ddd7a5ed
Davinder Kohli <prabhmeet.kohli@novamorph.com>
2026-05-27 22:40:59 -0400

    next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    staleTimes: {
+      dynamic: 0,
+      static: 0,
+    },
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Cache-Control", value: "no-store, max-age=0" },
+          { key: "CDN-Cache-Control", value: "no-store, max-age=0" },
+          { key: "Vercel-CDN-Cache-Control", value: "no-store, max-age=0" },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
